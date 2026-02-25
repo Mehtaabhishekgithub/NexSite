4b9897b26bd76f0d1c493b77db7eaef55fb836b1
Mehtaabhishekgithub <mehtaabhishek654@gmail.com>
2026-02-25 09:54:13 +0530

    Change CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://nexsite-1.onrender.com",
   credentials:true
 }))
 app.use("/api/auth",authRouter) 
